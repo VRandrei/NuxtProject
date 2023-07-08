27cead52d13e5d445d6f2959babbf51841342711
VRandrei <98388790+VRandrei@users.noreply.github.com>
2023-07-08 14:05:29 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div >
       <div class="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8 container">
           <div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-20 lg:text-left">
-            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Try the power of Next 3</h2>
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Try the power of Nuxt 3</h2>
             <p class="mt-6 text-lg leading-8 text-gray-900">A demo web application that will inspire you to create your own project!</p>
             <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
               <a href="#" class="rounded-md bg-gray-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"><NuxtLink to="/products">Get started</NuxtLink></a>
