8b9fd295fc41404349d288eb165a4024d14eedbb
VRandrei <98388790+VRandrei@users.noreply.github.com>
2023-07-10 17:07:48 +0300

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,13 @@
 <template>
-  <section class="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8 about">
-    <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">About Page</h1>
-    <p class="mt-6 text-lg leading-8 text-gray-900">Welcome to my Nuxt 3 JS app! I am glad to present you an example of using this framework.</p>
-    <p class="mt-6 text-lg leading-8 text-gray-900"> In this application, I've implemented routers to facilitate easy navigation. I've carefully designed the structure to ensure smooth transitions between different routes.</p>
-    <p class="mt-6 text-lg leading-8 text-gray-900"> I actively interact with APIs to retrieve and send data. Nuxt.js simplifies the process of creating and handling AJAX requests and displaying received information on the pages.</p>
-    <p class="mt-6 text-lg leading-8 text-gray-900"> With best wishes, Andrey</p>
-  </section>
+  <div>
+    <section class="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8 about">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">About Page</h1>
+      <p class="mt-6 text-lg leading-8 text-gray-900">Welcome to my Nuxt 3 JS app! I am glad to present you an example of using this framework.</p>
+      <p class="mt-6 text-lg leading-8 text-gray-900"> In this application, I've implemented routers to facilitate easy navigation. I've carefully designed the structure to ensure smooth transitions between different routes.</p>
+      <p class="mt-6 text-lg leading-8 text-gray-900"> I actively interact with APIs to retrieve and send data. Nuxt.js simplifies the process of creating and handling AJAX requests and displaying received information on the pages.</p>
+      <p class="mt-6 text-lg leading-8 text-gray-900"> With best wishes, Andrey</p>
+    </section>
+  </div>
 </template>
 
 <style scoped>
