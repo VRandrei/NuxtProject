59d8d69b5fa59b644012a33e0ab6a4bd5ef2c545
VRandrei <98388790+VRandrei@users.noreply.github.com>
2023-07-10 17:06:57 +0300

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,57 +1,60 @@
 <template>
-  <header>
-    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-      <div class="flex lg:flex-1">
-        <a href="#" class="-m-1.5 p-1.5">
-          <a class="text-sm font-semibold leading-6 text-gray-900"><NuxtLink to="/">Nuxt 3 SSR</NuxtLink></a>
-        </a>
-      </div>
-      <div class="flex lg:hidden">
-        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
-          <span class="sr-only">Open main menu</span>
-          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
-        </button>
-      </div>
-      <PopoverGroup class="hidden lg:flex lg:gap-x-12">
-        <a class="text-sm font-semibold leading-6 text-gray-900"><NuxtLink to="/">Home</NuxtLink></a>
-        <a class="text-sm font-semibold leading-6 text-gray-900"><NuxtLink to="/about">About</NuxtLink></a>
-        <a class="text-sm font-semibold leading-6 text-gray-900"><NuxtLink to="/products">Products</NuxtLink></a>
-      </PopoverGroup>
-      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-        <a href="#" class="text-sm font-semibold leading-6 text-gray-900"><NuxtLink to="/login">Log in</NuxtLink><span aria-hidden="true">&rarr;</span></a>
-      </div>
-    </nav>
-    <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
-      <div class="fixed inset-0 z-10" />
-      <DialogPanel class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-        <div class="flex items-center justify-between">
-          <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Nuxt 3</span>
-            <a class="text-sm font-semibold leading-6 text-gray-900"><NuxtLink to="/">Nuxt 3 SSR</NuxtLink></a>
-          </a>
-          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
-            <span class="sr-only">Close menu</span>
-            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div class="mt-6 flow-root">
-          <div class="-my-6 divide-y divide-gray-500/10">
-            <div class="space-y-2 py-6">
-              <NuxtLink to="/"><a class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Home</a></NuxtLink>
-              <NuxtLink to="/about"><a class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About</a></NuxtLink>
-              <NuxtLink to="/products"><a class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Products</a></NuxtLink>
-            </div>
-            <div class="py-6">
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"><NuxtLink to="/login">Log in</NuxtLink></a>
-            </div>
+  <div>
+    <header>
+      <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div class="flex lg:flex-1">
+          <div class="-m-1.5 p-1.5">
+            <NuxtLink class="text-sm font-semibold leading-6 text-gray-900" to="/">Nuxt 3 SSR</NuxtLink>
           </div>
         </div>
-      </DialogPanel>
-    </Dialog>
-  </header>
+        <div class="flex lg:hidden">
+          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <PopoverGroup class="hidden lg:flex lg:gap-x-12">
+          <NuxtLink class="text-sm font-semibold leading-6 text-gray-900" to="/">Home</NuxtLink>
+          <NuxtLink class="text-sm font-semibold leading-6 text-gray-900" to="/about">About</NuxtLink>
+          <NuxtLink class="text-sm font-semibold leading-6 text-gray-900" to="/products">Products</NuxtLink>
+        </PopoverGroup>
+        <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+          <NuxtLink class="text-sm font-semibold leading-6 text-gray-900" to="/login">Log in<span>&rarr;</span></NuxtLink>
+        </div>
+      </nav>
+     <ClientOnly>
+      <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+        <div class="fixed inset-0 z-10" />
+          <DialogPanel class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div class="flex items-center justify-between">
+              <div class="-m-1.5 p-1.5">
+                <NuxtLink class="text-sm font-semibold leading-6 text-gray-900" to="/">Nuxt 3 SSR</NuxtLink>
+              </div>
+              <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+                <span class="sr-only">Close menu</span>
+                <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div class="mt-6 flow-root">
+              <div class="-my-6 divide-y divide-gray-500/10">
+                <div class="space-y-2 py-6">
+                  <NuxtLink class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" to="/">Home</NuxtLink>
+                  <NuxtLink class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" to="/about">About</NuxtLink>
+                  <NuxtLink class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" to="/products">Products</NuxtLink>
+                </div>
+                <div class="py-6">
+                  <NuxtLink class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" to="/login">Log in</NuxtLink>
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+      </Dialog>
+     </ClientOnly>
+    </header>
 
-  <div>
-    <slot/>
+    <div>
+      <slot/>
+    </div>
   </div>
 </template>
 
